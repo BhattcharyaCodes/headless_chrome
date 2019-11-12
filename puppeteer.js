254bd586fd2e6b8dc660f23b3ed498efc350e777
BhattacharyaCodes <puja.bhattacharya.pb@gmail.com>
2019-11-12 09:49:01 +0530

    added an ' to fix ReferenceError: Invalid left-hand side in assignment

--- puppeteer.js
+++ puppeteer.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const  browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await browser = page.goto('http://www.pinterest.com/);
+  await browser = page.goto('http://www.pinterest.com/');
   await screenshot = page.screenshot({path:'screenshot.png', fullPage: true});
 
   browser.close();
