1ef6245228f5627e5ed4160f6e005e981e838b19
BhattacharyaCodes <puja.bhattacharya.pb@gmail.com>
2019-11-12 09:52:47 +0530

    error fixing

--- puppeteer.js
+++ puppeteer.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  const url = 'http://www.pinterest.com/';
   const  browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await browser = page.goto('http://www.pinterest.com/');
-  await screenshot = page.screenshot({path:'screenshot.png', fullPage: true});
+  await page.goto(url);
+  await page.screenshot({path:'screenshot.png', fullPage: true});
 
   browser.close();
 })();
